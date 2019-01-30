ba7f26458e0f2eb33ff122c5c157c9e15f391365
Stephan Beier <stbkde@gmail.com>
2019-01-30 14:07:21 +0100

    Create hserv_setup1.sh

---
+++ hserv_setup1.sh
@@ -0,0 +1,39 @@
+echo "Base setup script for hserv (Thinkpad T60)"
+
+read -p "Continue and format the whole disc? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Preparing setup..."
+    timedatectl set-ntp true
+
+    echo "Partition the hard drive"
+    parted /dev/sda --script -- mklabel gpt
+
+    parted /dev/sda --script -- mkpart primary ext4 1MiB 25GiB
+    parted /dev/sda --script -- set 1 boot on
+    parted /dev/sda --script -- mkpart primary linux-swap 25.5GiB 33.5GiB
+    parted /dev/sda --script -- mkpart primary 28.5GiB 100%
+
+    echo "Make filesystems p_arch and p_home"
+    mkfs.ext4 -L p_arch /dev/sda2
+    mkfs.btrfs -L p_home /dev/sda3
+    
+    echo "Configuring swap..."
+    mkswap -L p_swap /dev/sda1
+    swapon -L p_swap
+    
+    echo "Mounting filesystems..."
+    # root
+    mount /dev/sda2 /mnt
+
+    # home
+    mkdir /mnt/home
+    mount /dev/sda3 /mnt/home
+    
+    mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+    grep -E -A 1 ".*Germany.*$" /etc/pacman.d/mirrorlist.bak | sed '/--/d' > /etc/pacman.d/mirrorlist
+    
+    echo "Installation of base system is running..."
+    pacstrap /mnt base base-devel
+fi
