41ff79d26fce2989b96447e90d57a5337f4deb8c
Stephan Beier <stbkde@gmail.com>
2019-01-31 10:44:14 +0100

    Update hserv_setup_chroot.sh

--- hserv_setup_chroot.sh
+++ hserv_setup_chroot.sh
@@ -1,7 +1,7 @@
 echo "Before starting change to root on /mnt: "
 echo "» arch-chroot /mnt"
 
-read -p "Continue and format the whole disc? " -n 1 -r
+read -p "Continue with setup? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
